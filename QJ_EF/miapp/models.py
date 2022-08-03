96a7f42db8b437b3d07664a2c866cab679b3f8d9
Humberto Julca <humbertojc120@gmail.com>
2022-08-03 18:57:03 -0500

    EF_Parte02-Producto - Julca

--- QJ_EF/miapp/models.py
+++ QJ_EF/miapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class producto(models.Model):
+    codigo= models.TextField(max_length=10)
+    nombre    = models.TextField()
+    precio_compra    = models.TextField()
+    precio_venta    = models.TextField()
+    Fecha_compra = models.TextField()
+    Fecha_registro = models.TextField()
+    estado   = models.CharField(max_length=1)
--- QJ_EF/miapp/views.py
+++ QJ_EF/miapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, HttpResponse
+from miapp.models import producto
+from datetime import datetime
 
 # Create your views here.
 def integrantes(request):
@@ -16,10 +18,24 @@ def saludo(request):
     })
 
 def crear_producto(request):
+    productos=producto(
+    codigo          = "5",
+    nombre          = "limon",
+    precio_compra   = "3",
+    precio_venta    = "4",
+    Fecha_compra    = f" {datetime.today().strftime('%Y-%m-%d')}",
+    Fecha_registro = f" {datetime.today().strftime('%Y-%m-%d')}",
+    estado   = "A"
+    )
+    productos.save()
 
     return render(request, 'crear_producto.html', {
         'titulo':'Crear producto',
-        'producto':""
+        'producto':
+        f"Producto registrado:"+
+        f"//codigo: {productos.codigo} // nombre: {productos.nombre} // precio_compra: {productos.precio_compra} //"+
+        f"precio_venta: {productos.precio_venta} // Fecha_compra: {productos.Fecha_compra} // Fecha_de_registro: {productos.Fecha_registro}"+
+        f"// Estado:{productos.estado}"
     })
 
 def crear_curso(request):
